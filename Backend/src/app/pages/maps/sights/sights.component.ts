1f2dbd78989ea0aea21911b48bb426e28838af10
Safie Ur Rahman <13besesrehman@seecs.edu.pk>
2019-12-19 12:59:55 +0100

    Develop route using polyline implemented. From created for routes with some fields.

--- Backend/src/app/pages/maps/sights/sights.component.ts
+++ Backend/src/app/pages/maps/sights/sights.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MouseEvent } from '@agm/core';
+import { marker } from 'leaflet';
+import { Location } from '../search-map/entity/Location'; // for future implementation of search bar
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'ngx-sights',
   styleUrls: ['./sights.component.scss'],
@@ -8,27 +13,78 @@ import { MouseEvent } from '@agm/core';
 })
 export class SightsComponent {
 
+  zoom: number = 15;
+
   lat = 49.239486;
   lng = 6.994886; 
 
   markers:marker[] = [];
   index: string  = 'M';
-  i:number = 0;
+  //i:number = 0;
+
+  routeForm: FormGroup;
+  
+  constructor(private formBuilder: FormBuilder,
+              private http: HttpClient,
+    ) {
+    this.createrouteForm();
+  }
+
+  routeName = new FormControl([
+    //Validators.required,
+  ]);
+
+  routeDescription = new FormControl();
+  routeType = new FormControl();
+
+  createrouteForm(){
+    this.routeForm = this.formBuilder.group({
+      routeName: [''],  
+      routeDescription: [''],
+      routeType: ['']
+    });
+  }
+
+  ngOnInit(): void {
+  }
   
   clickedMarker(label: string, index: number) 
   {
-    console.log(`clicked the marker: ${label || index}`);
+    this.markers.splice(index,1);
+    console.log(`Deleted the marker: ${label || index}`);
   }
   
   mapClicked($event: MouseEvent) {
     this.markers.push({
       lat: $event.coords.lat,
       lng: $event.coords.lng,
-      label: this.index+this.i++,
-      draggable: true,
+      //label: this.index+this.i++,
+      label: this.index,
+      draggable: false,
     });
     console.log (this.markers);
   }
+
+
+  public onSubmit() {
+    {
+    if(this.routeForm.valid)
+      {
+  
+      let new_route = {};
+        new_route["name"]=this.routeForm.value.roueName;
+        new_route["description"]=this.routeForm.value.routeDescription;
+        new_route["type"]=this.routeForm.value.routeType;
+        //new_route["longiitude"]=this.longitude;
+        //new_route["latitude"]=this.latitude;
+      
+      console.log('Form data is : ', this.routeForm.value );
+      }
+    }
+  }
+  
+
+  
 
 }
 
