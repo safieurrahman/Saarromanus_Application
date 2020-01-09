bd961889241865ed82a97bbcea130fb86ffb2795
taqi457 <taqi457@gmail.com>
2020-01-09 13:24:16 +0100

    remove social links from the footer as well

--- Backend/src/app/@theme/components/footer/footer.component.ts
+++ Backend/src/app/@theme/components/footer/footer.component.ts
@@ -5,12 +5,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
   template: `
     <span class="created-by">Created as SE Project by Group 27</span>
-    <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-github"></a>
-      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
-      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
-      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
-    </div>
   `,
 })
 export class FooterComponent {
