9e58b6eb215be47337040766fbb5b78511ed1298
Kazi Fozle Azim Rabi <s8karabi@stud.uni-saarland.de>
2020-01-15 01:56:30 +0100

    Update action and reducer

--- SmartphoneApps/Saarromanus/src/actions/app-config.js
+++ SmartphoneApps/Saarromanus/src/actions/app-config.js
@@ -3,6 +3,8 @@ export const INVOKE_TOGGLE_CHECK_UPDATE = 'INVOKE_TOGGLE_CHECK_UPDATE';
 export const TOGGLE_CHECK_UPDATE = 'TOGGLE_CHECK_UPDATE';
 export const SHOW_LOADING_SCREEN = 'SHOW_LOADING_SCREEN';
 export const HIDE_LOADING_SCREEN = 'HIDE_LOADING_SCREEN';
+export const SHOW_ALERT = 'SHOW_ALERT';
+export const HIDE_ALERT = 'HIDE_ALERT';
 
 export const detectCheckUpdate = () => {
 	return { type: DETECT_CHECK_UPDATE };
@@ -22,4 +24,12 @@ export const showLoadingScreen = () => {
 
 export const hideLoadingScreen = () => {
 	return { type: HIDE_LOADING_SCREEN };
+};
+
+export const hideAlert = () => {
+	return { type: HIDE_ALERT };
+};
+
+export const showAlert = payload => {
+	return { type: SHOW_ALERT, payload };
 };
