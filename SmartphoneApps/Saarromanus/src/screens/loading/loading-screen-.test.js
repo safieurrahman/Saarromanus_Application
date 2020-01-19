058a9f6e539ec0a2de4535f4536f476629e5d793
Tanjim Ul Haque <s8tahaqu@stud.uni-saarland.de>
2020-01-19 19:44:35 +0100

    Add test for non laoding state

--- SmartphoneApps/Saarromanus/src/screens/loading/loading-screen-.test.js
+++ SmartphoneApps/Saarromanus/src/screens/loading/loading-screen-.test.js
@@ -16,3 +16,23 @@ const initialLoadingState = {
 const initialNonLoadingState = {
 	config: { loading: false },
 };
+
+describe('<LoadingScreen />', () => {
+	describe('If Not Loading', () => {
+		let wrapper;
+		let store;
+
+		beforeAll(() => {
+			store = mockStore(initialNonLoadingState);
+			wrapper = shallow(<LoadingScreen store={store} />).dive();
+		});
+
+		it('should not render', () => {
+			expect(wrapper).toMatchSnapshot('loading:false');
+		});
+
+		it('should not mount any <View /> component', () => {
+			expect(wrapper.find('View').length).toBe(0);
+		});
+	});
+});
