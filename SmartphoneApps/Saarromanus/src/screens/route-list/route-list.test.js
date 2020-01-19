accbfef065e39d4c3d9466a1478f20d0f5468952
Kazi Fozle Azim Rabi <s8karabi@stud.uni-saarland.de>
2020-01-19 11:48:52 +0100

    Add intial state for route list screen tests

---
+++ SmartphoneApps/Saarromanus/src/screens/route-list/route-list.test.js
@@ -0,0 +1,48 @@
+import 'react-native';
+import React from 'react';
+import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
+
+import RouteListScreen from './component';
+
+const mockStore = configureStore();
+
+const mockFn = jest.fn();
+const navigation = {
+	navigate: mockFn,
+};
+
+const initialState = {
+	routes: [
+		{
+			id: 'KBaeZFpmqRafsXhcsq3V',
+			en: {
+				name: 'Dillingen Route',
+				information: 'Info..',
+			},
+			de: {
+				name: 'Dillingen Route',
+				information: 'Info..',
+			},
+			fr: {
+				name: 'Delingen Route',
+				information: 'Info..',
+			},
+		},
+		{
+			id: 'lzOL126R7Em8FDO0jlHH',
+			en: {
+				name: 'Saarland Route',
+				information: 'Information',
+			},
+			de: {
+				name: 'Saarland Route',
+				information: 'Informationen',
+			},
+			fr: {
+				information: 'Informations',
+				name: 'Route de la Sarre',
+			},
+		},
+	],
+};
