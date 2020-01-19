1e6a4221816f7ac54de4725065f5094c08690dfd
Kazi Fozle Azim Rabi <s8karabi@stud.uni-saarland.de>
2020-01-19 12:41:23 +0100

    RouteList: add test for single list item

--- SmartphoneApps/Saarromanus/src/screens/route-list/route-list.test.js
+++ SmartphoneApps/Saarromanus/src/screens/route-list/route-list.test.js
@@ -65,7 +65,36 @@ describe('<RouteListScreen />', () => {
 		expect(wrapper).toMatchSnapshot();
 	});
 
-	it('has 1 main <View /> Component', () => {
+	it('has one main <View /> component', () => {
 		expect(component.find(`[data-test-id='mainComponent']`).length).toBe(1);
+	});
+
+	describe('Routes List', () => {
+		it('length should not be zero when there are routes', () => {
+			expect(component.find(`[data-test-id='listItem']`).length).not.toBe(
+				0
+			);
+		});
+		it('length should be equal to the number of routes received from the store', () => {
+			expect(component.find(`[data-test-id='listItem']`).length).toBe(
+				initialState.routes.length
+			);
+		});
+		describe('Single Route', () => {
+			it('should navigate to the Route Details page when pressed/tapped', () => {
+				component
+					.find(`[data-test-id='listItem']`)
+					.find('TouchableOpacity')
+					.forEach(node => {
+						node.simulate('press');
+					});
+				expect(mockFn).toBeCalled();
+				expect(mockFn.mock.calls.length).toBe(2);
+			});
+			it('should not fire navigation event if it is not pressed/tapped', () => {
+				expect(jest.fn()).not.toBeCalled();
+				expect(jest.fn().mock.calls.length).toBe(0);
+			});
+		});
 	});
 });
