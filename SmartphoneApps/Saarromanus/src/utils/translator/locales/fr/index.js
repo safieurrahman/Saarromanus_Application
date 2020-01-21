971ae628db9fab973ad32556a1b2cbf4322b4a82
Kazi Fozle Azim Rabi <s8karabi@stud.uni-saarland.de>
2020-01-21 19:42:06 +0100

    Update English locales

--- SmartphoneApps/Saarromanus/src/utils/translator/locales/fr/index.js
+++ SmartphoneApps/Saarromanus/src/utils/translator/locales/fr/index.js
@@ -1,32 +1,12 @@
 const en = {
 	'en-US': 'English',
 	'de-DE': 'German',
-	'fr-FR': 'French',
-
-	// Index Screen
 	routes: 'ROUTES',
 	sights: 'SIGHTS',
 	game: 'GAME',
-
-	// Settings Screen
-	settings: 'Settings',
-	changeLanguage: 'Change Language',
-	checkOfflineContentsUpdate: "Check For Offline Contents' Update",
 	userGuildeline: 'User Guideline',
-
-	// Routes Screen
-
-	// Sight Categoris
-	sightCategories: 'Sight Categories',
-
-	// Sights List Screen
-	sightsList: 'Sights List',
-
-	// Sight Details Screen
-	sightDetails: 'Sight Details',
-	moreAbout: 'More About',
-
-	// dev
+	changeLanguage: 'Change Language',
+	settings: 'Settings',
 	loremText: `Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.`,
 	// foo: 'Bar {{someValue}}',
 };
