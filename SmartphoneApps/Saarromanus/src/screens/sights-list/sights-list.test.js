1b69a0f6b88baa35aa000e029b671630ab4c3ab5
Krunal Jagdishbhai Koshiya <s8krkosh@stud.uni-saarland.de>
2020-01-21 14:45:00 +0100

    Add initial state

---
+++ SmartphoneApps/Saarromanus/src/screens/sights-list/sights-list.test.js
@@ -0,0 +1,37 @@
+import 'react-native';
+import React from 'react';
+import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
+
+import SightsListScreen from './index';
+
+const mockStore = configureStore();
+
+const mockFn = jest.fn();
+const navigation = {
+	navigate: mockFn,
+};
+
+const initialState = {
+	config: { checkForUpdate: false },
+	sights: [
+		{
+			id: 'AK1AIRDavlLPkNjV1VDt',
+			en: {
+				name: 'Mithra chapel on Halberg',
+				information: 'The worship of the sun...',
+			},
+			de: {
+				name: 'Mithraskapelle am Halberg',
+				information: 'Die Anbetung der Sonne ist ein Kult',
+			},
+			fr: {
+				name: 'Chapelle de Mithras à Halberg',
+				information: "L'adoration du soleil est un culte",
+			},
+			thumbnail: 'https://firebasestorage.googleapis.com/v0/b',
+			resourceName: 'front_large.jpg',
+			sight_category: 'qXG2ZqlHWLQiDLYea1qn',
+		},
+	],
+};
