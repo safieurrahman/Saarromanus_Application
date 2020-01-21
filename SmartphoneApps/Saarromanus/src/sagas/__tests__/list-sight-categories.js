2107bede7c925b4f61c01a91cab3a29058d2b964
Krunal Jagdishbhai Koshiya <s8krkosh@stud.uni-saarland.de>
2020-01-21 15:08:05 +0100

    Add relevent tests for all situations

--- SmartphoneApps/Saarromanus/src/sagas/__tests__/list-sight-categories.js
+++ SmartphoneApps/Saarromanus/src/sagas/__tests__/list-sight-categories.js
@@ -62,3 +62,62 @@ const mockedSightCategories = [
 		},
 	},
 ];
+
+let mockedResponse;
+let fakeStore;
+
+beforeAll(() => {
+	mockedResponse = { payload: mockedSightCategories, success: true };
+});
+
+describe('list-sights-categories-saga', () => {
+	let dispatchedActions;
+
+	beforeAll(async () => {
+		getSightCategories.mockImplementation(() =>
+			Promise.resolve(mockedResponse)
+		);
+		fakeStore = configureStore()({});
+		await runSaga(fakeStore, fetchSightCategoriesSaga).done;
+		dispatchedActions = fakeStore.getActions();
+	});
+
+	it('should dispatch the show loading screen event before making any request', () => {
+		expect(dispatchedActions[0]).toEqual(showLoadingScreen());
+	});
+
+	it('should make a GET request to /sight_categories end point', () => {
+		expect(getSightCategories.mock.calls.length).toBe(1);
+	});
+
+	it('should dispatch the hide loading screen event irrespective of the response', () => {
+		expect(dispatchedActions[2]).toEqual(hideLoadingScreen());
+	});
+
+	describe('SUCCESS', () => {
+		it('should dispatch a populate sight_categories event', () => {
+			expect(dispatchedActions[1]).toEqual(
+				populateSightCategories(mockedSightCategories)
+			);
+		});
+	});
+
+	describe('ERROR', () => {
+		let failedDispatchedActions = [];
+		beforeAll(async () => {
+			getSightCategories.mockImplementation(() => Promise.reject(''));
+			const failedStore = configureStore()({});
+			await runSaga(failedStore, fetchSightCategoriesSaga).done;
+			failedDispatchedActions = failedStore.getActions();
+		});
+		it('should show network request failed related alert', () => {
+			expect(failedDispatchedActions[2]).toEqual(
+				showAlert({
+					title: 'Network Request Failed!',
+					message:
+						'Unable to load data from the server. Please connect to the internet and try again if you are not connected already.',
+				})
+			);
+		});
+	});
+});
