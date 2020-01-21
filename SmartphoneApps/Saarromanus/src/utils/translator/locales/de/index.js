51d43115cdd05873a1586b7e3dffe8e10690b840
Kazi Fozle Azim Rabi <s8karabi@stud.uni-saarland.de>
2020-01-21 19:48:58 +0100

    Update German locales

--- SmartphoneApps/Saarromanus/src/utils/translator/locales/de/index.js
+++ SmartphoneApps/Saarromanus/src/utils/translator/locales/de/index.js
@@ -1,12 +1,33 @@
 const de = {
 	'en-US': 'Englisch',
 	'de-DE': 'Deutsch',
+	'fr-FR': 'Französisch',
+
+	// Index Screen
 	routes: 'ROUTEN',
 	sights: 'SEHENSWÜRDIGKEITEN',
 	game: 'SPIEL',
-	userGuildeline: 'Bedienungsanleitung',
-	changeLanguage: 'Sprache Ändern',
+
+	// Settings Screen
 	settings: 'Einstellungen',
+	changeLanguage: 'Sprache Ändern',
+	userGuildeline: 'Bedienungsanleitung',
+	checkOfflineContentsUpdate:
+		'Überprüfen Sie, ob das Update für Offline-Inhalte verfügbar ist',
+
+	// Routes Screen
+
+	// Sight Categoris
+	sightCategories: 'Sichtkategorien',
+
+	// Sights List Screen
+	sightsList: 'Liste der Sehenswürdigkeiten',
+
+	// Sight Details Screen
+	sightDetails: 'Details zum Anblick',
+	moreAbout: 'Mehr über',
+
+	// dev
 	loremText: `Lorem Ipsum oder Lipsum, wie es manchmal genannt wird, ist eine Attrappe Text für die Gestaltung von Druck-, Grafik- oder Webdesigns. Das Passage wird im 15. einem unbekannten Setzer zugeschrieben Jahrhundert, von dem angenommen wird, dass sie Teile von Cicero's durcheinandergebracht haben De Finibus Bonorum et Malorum zur Verwendung in einem Muster Buch. Lorem ipsum oder Lipsum, wie es manchmal genannt wird, ist Dummy-Text für das Layout von Druck-, Grafik- oder Webdesigns. Die Passage wird einem unbekannten Setzer im zugeschrieben 15. Jahrhundert, von dem angenommen wird, dass sie Teile von Ciceros De Finibus Bonorum et Malorum zur Verwendung in einem Typ Musterbuch.`,
 };
 
