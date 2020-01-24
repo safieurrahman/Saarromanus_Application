e9c4aaab8d32f0095f0dce4db423ee5372b574d5
Safie Ur Rahman <13besesrehman@seecs.edu.pk>
2020-01-24 19:30:44 +0100

    Test Fix for User Manual

--- Backend/src/app/pages/instruction/instruction.component.spec.ts
+++ Backend/src/app/pages/instruction/instruction.component.spec.ts
@@ -2,7 +2,7 @@ import { TestBed, async } from '@angular/core/testing';
 import { InstructionComponent } from './instruction.component';
 import { RouterTestingModule } from '@angular/router/testing';
 
-describe('SightCategoriesComponent', () => {
+describe('InstructionComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
         imports: [    
@@ -14,24 +14,9 @@ describe('SightCategoriesComponent', () => {
     }).compileComponents();
   }));
 
-  it('Should render the Sight Categories Module', async(() => {
+  it('Should render the Instruction Module', async(() => {
     const fixture = TestBed.createComponent(InstructionComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-
-  /*
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
-  }));
-  */
 });
